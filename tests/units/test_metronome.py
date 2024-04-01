f8c4dd147cffff435238b45986afb64cc1497891
Evgeniy Blinov <zheni-b@yandex.ru>
2024-04-01 14:50:31 +0300

    new tests

--- tests/units/test_metronome.py
+++ tests/units/test_metronome.py
@@ -1,3 +1,4 @@
+from time import sleep
 from threading import active_count
 
 import pytest
@@ -32,3 +33,20 @@ def test_by_default_it_is_creating_a_thread():
 
     metronome.stop()
     assert count_before == active_count()
+
+
+def test_alternation_of_sleep_and_callback():
+    actions = []
+
+    metronome = Metronome(0.0001, lambda: actions.append(1), sleeping_callback=lambda: actions.append(2))
+    metronome.start()
+
+    sleep(0.1)
+
+    metronome.stop()
+
+    for index, action in enumerate(actions):
+        if index % 2 == 0:
+            assert action == 1
+        else:
+            assert action == 2
