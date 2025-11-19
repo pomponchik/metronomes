4a33ca1abb8b44544a0f4d604c0ffa7638f3eea8
pomponchik <zheni-b@yandex.ru>
2025-11-20 00:27:14 +0700

    Newer dependencies versions

--- tests/units/test_metronome.py
+++ tests/units/test_metronome.py
@@ -1,18 +1,23 @@
-from time import sleep
 from copy import copy
 from threading import active_count
+from time import sleep
 
 import pytest
-import full_match
-from emptylog import MemoryLogger
 from cantok import SimpleToken
+from emptylog import MemoryLogger
+from full_match import match
 
 from metronomes import Metronome
-from metronomes.errors import RunStoppedMetronomeError, RunAlreadyStartedMetronomeError, StopNotStartedMetronomeError, StopStoppedMetronomeError
+from metronomes.errors import (
+    RunAlreadyStartedMetronomeError,
+    RunStoppedMetronomeError,
+    StopNotStartedMetronomeError,
+    StopStoppedMetronomeError,
+)
 
 
 @pytest.mark.parametrize(
-    'iteration',
+    "iteration",
     [
         0,
         0.0,
@@ -22,7 +27,12 @@ from metronomes.errors import RunStoppedMetronomeError, RunAlreadyStartedMetrono
     ],
 )
 def test_iteration_time_zero_or_less(iteration):
-    with pytest.raises(ValueError, match=full_match('The duration of the metronome iteration (tick-tock time) must be greater than zero.')):
+    with pytest.raises(
+        ValueError,
+        match=match(
+            "The duration of the metronome iteration (tick-tock time) must be greater than zero."
+        ),
+    ):
         Metronome(iteration, lambda: None)
 
 
@@ -42,7 +52,11 @@ def test_by_default_it_is_creating_a_thread():
 def test_alternation_of_sleep_and_callback():
     actions = []
 
-    metronome = Metronome(0.1, lambda: (actions.append(1), sleep(0.0001)), sleeping_callback=lambda x: actions.append(2))
+    metronome = Metronome(
+        0.1,
+        lambda: (actions.append(1), sleep(0.0001)),
+        sleeping_callback=lambda x: actions.append(2),
+    )
     metronome.start()
 
     sleep(0.1)
@@ -63,7 +77,10 @@ def test_start_started():
 
     metronome.start()
 
-    with pytest.raises(RunAlreadyStartedMetronomeError, match=full_match('The metronome has already been launched.')):
+    with pytest.raises(
+        RunAlreadyStartedMetronomeError,
+        match=match("The metronome has already been launched."),
+    ):
         metronome.start()
 
     metronome.stop()
@@ -75,7 +92,12 @@ def test_stop_stopped():
     metronome.start()
     metronome.stop()
 
-    with pytest.raises(StopStoppedMetronomeError, match=full_match("You've already stopped this metronome (or it was canceled on its own, for example, when the limit expired), it's impossible to do it twice.")):
+    with pytest.raises(
+        StopStoppedMetronomeError,
+        match=match(
+            "You've already stopped this metronome (or it was canceled on its own, for example, when the limit expired), it's impossible to do it twice."
+        ),
+    ):
         metronome.stop()
 
 
@@ -85,14 +107,22 @@ def test_start_stopped():
     metronome.start()
     metronome.stop()
 
-    with pytest.raises(RunStoppedMetronomeError, match=full_match('Metronomes are disposable, you cannot restart a stopped metronome.')):
+    with pytest.raises(
+        RunStoppedMetronomeError,
+        match=match(
+            "Metronomes are disposable, you cannot restart a stopped metronome."
+        ),
+    ):
         metronome.start()
 
 
 def test_stop_not_started():
     metronome = Metronome(0.0001, lambda: None)
 
-    with pytest.raises(StopNotStartedMetronomeError, match=full_match("You can't stop a metronome that hasn't been started yet.")):
+    with pytest.raises(
+        StopNotStartedMetronomeError,
+        match=match("You can't stop a metronome that hasn't been started yet."),
+    ):
         metronome.stop()
 
 
@@ -105,7 +135,9 @@ def test_start_returns_the_same_metronome():
 
 
 def test_normal_logs_order():
-    def callback(): pass
+    def callback():
+        pass
+
     logger = MemoryLogger()
     metronome = Metronome(0.0001, callback, logger=logger)
 
@@ -114,7 +146,7 @@ def test_normal_logs_order():
     metronome.start()
 
     assert len(logger.data.info) == 1
-    assert logger.data.info[0].message == 'The metronome starts...'
+    assert logger.data.info[0].message == "The metronome starts..."
 
     sleep(0.0001 * 10)
 
@@ -123,19 +155,28 @@ def test_normal_logs_order():
 
     for index, log_item in enumerate(copy(logger.data.debug)):
         if index % 2 == 0:
-            assert log_item.message == 'The beginning of the execution of callback "callback".'
+            assert (
+                log_item.message
+                == 'The beginning of the execution of callback "callback".'
+            )
         else:
-            assert log_item.message == 'Callback "callback" has been successfully completed.'
+            assert (
+                log_item.message
+                == 'Callback "callback" has been successfully completed.'
+            )
 
     metronome.stop()
 
     assert len(logger.data.info) == 2
-    assert logger.data.info[1].message == 'The metronome has stopped.'
+    assert logger.data.info[1].message == "The metronome has stopped."
 
 
 def test_cancellation_token_is_stopping_the_metronome():
     sleep_time = 0.0001
-    def callback(): pass
+
+    def callback():
+        pass
+
     logger = MemoryLogger()
     token = SimpleToken()
     metronome = Metronome(sleep_time, callback, logger=logger, token=token)
@@ -155,7 +196,10 @@ def test_cancellation_token_is_stopping_the_metronome():
 
 def test_cancellation_token_is_stopping_the_metronome_in_start_method():
     sleep_time = 0.0001
-    def callback(): pass
+
+    def callback():
+        pass
+
     logger = MemoryLogger()
     token = SimpleToken()
     metronome = Metronome(sleep_time, callback, logger=logger)
@@ -174,7 +218,7 @@ def test_cancellation_token_is_stopping_the_metronome_in_start_method():
 
 
 @pytest.mark.parametrize(
-    ['first_token', 'second_token'],
+    ["first_token", "second_token"],
     [
         (SimpleToken(), SimpleToken(cancelled=True)),
         (SimpleToken(cancelled=True), SimpleToken()),
@@ -184,7 +228,9 @@ def test_cancellation_token_is_stopping_the_metronome_in_start_method():
 def test_start_with_cancelled_token(first_token, second_token):
     actions = []
     logger = MemoryLogger()
-    metronome = Metronome(0.0001, lambda: actions.append(True), token=first_token, logger=logger)
+    metronome = Metronome(
+        0.0001, lambda: actions.append(True), token=first_token, logger=logger
+    )
 
     metronome.start(token=second_token)
     sleep(0.0001)
@@ -193,18 +239,30 @@ def test_start_with_cancelled_token(first_token, second_token):
     assert not actions
     assert len(logger.data) == 2
     assert len(logger.data.info) == 2
-    assert logger.data.info[0].message == 'The metronome starts...'
-    assert logger.data.info[1].message == 'The metronome did not start working because the cancellation token was canceled right at the start.'
+    assert logger.data.info[0].message == "The metronome starts..."
+    assert (
+        logger.data.info[1].message
+        == "The metronome did not start working because the cancellation token was canceled right at the start."
+    )
 
 
 def test_behavior_when_the_callback_time_is_bigger_than_loop_time():
     actions = []
     loop_time = 0.0001
     sleep_time = loop_time * 5
-    def callback(): (sleep(sleep_time), actions.append(1))
+
+    def callback():
+        (sleep(sleep_time), actions.append(1))
+
     logger = MemoryLogger()
     token = SimpleToken()
-    metronome = Metronome(loop_time, callback, logger=logger, token=token, sleeping_callback=lambda x: actions.append(2))
+    metronome = Metronome(
+        loop_time,
+        callback,
+        logger=logger,
+        token=token,
+        sleeping_callback=lambda x: actions.append(2),
+    )
 
     metronome.start()
 
@@ -218,13 +276,18 @@ def test_behavior_when_the_callback_time_is_bigger_than_loop_time():
     assert len(logger.data.warning)
 
     for log_item in logger.data.warning:
-        assert log_item.message.startswith('The callback worked for more than the amount of time allocated for one iteration. The extra time was ')
-        assert log_item.message.endswith(' seconds.')
+        assert log_item.message.startswith(
+            "The callback worked for more than the amount of time allocated for one iteration. The extra time was "
+        )
+        assert log_item.message.endswith(" seconds.")
 
 
 def test_exceptions_escaping():
     sleep_time = 0.0001
-    def callback(): raise ValueError('kek')
+
+    def callback():
+        raise ValueError("kek")
+
     logger = MemoryLogger()
     metronome = Metronome(sleep_time, callback, logger=logger)
 
@@ -238,22 +301,42 @@ def test_exceptions_escaping():
 
 
 def test_iteration_more_than_duration():
-    with pytest.raises(ValueError, match=full_match('The time of one iteration cannot exceed the running time of the metronome as a whole.')):
+    with pytest.raises(
+        ValueError,
+        match=match(
+            "The time of one iteration cannot exceed the running time of the metronome as a whole."
+        ),
+    ):
         Metronome(5, lambda: None, duration=1)
 
 
 def test_iteration_more_than_duration_in_start_method():
-    with pytest.raises(ValueError, match=full_match('The time of one iteration cannot exceed the running time of the metronome as a whole.')):
+    with pytest.raises(
+        ValueError,
+        match=match(
+            "The time of one iteration cannot exceed the running time of the metronome as a whole."
+        ),
+    ):
         Metronome(5, lambda: None).start(duration=1)
 
 
 def test_duration_less_than_zero():
-    with pytest.raises(ValueError, match=full_match('The total duration of the metronome operation cannot be less than zero.')):
+    with pytest.raises(
+        ValueError,
+        match=match(
+            "The total duration of the metronome operation cannot be less than zero."
+        ),
+    ):
         Metronome(0.5, lambda: None, duration=-1)
 
 
 def test_duration_less_than_zero_in_start_method():
-    with pytest.raises(ValueError, match=full_match('The total duration of the metronome operation cannot be less than zero.')):
+    with pytest.raises(
+        ValueError,
+        match=match(
+            "The total duration of the metronome operation cannot be less than zero."
+        ),
+    ):
         Metronome(0.5, lambda: None).start(duration=-1)
 
 
@@ -323,9 +406,9 @@ def test_stop_metronome_into_context_manager():
 def test_raise_into_context_manager():
     metronome = Metronome(0.00001, lambda: None)
 
-    with pytest.raises(ValueError, match=full_match('text')):
+    with pytest.raises(ValueError, match=match("text")):
         with metronome:
-            raise ValueError('text')
+            raise ValueError("text")
 
 
 def test_numbers_of_threads_into_context_manager():
